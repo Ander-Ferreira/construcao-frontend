0144f289b80ce46a7d976a7691db4dc47f091c2b
Anderson Ferreira <andersonadm1314@gmail.com>
2024-09-01 21:22:53 -0300

    Commit inicial,  page.js inicial, instalação do bootstrap, gerando componentes dinâmicos com Pagina.js e Cabecalho.js em Component.

---
+++ Aprendizado/fundamentos-comentados/fundamentos/src/app/components/Pagina.js
@@ -0,0 +1,40 @@
+import { Container, Nav, Navbar } from "react-bootstrap";
+
+export default function Pagina(props){
+
+
+    return(
+
+        <>
+       <Navbar bg="dark" data-bs-theme="dark">
+                <Container>
+                    <Navbar.Brand href="/">Fundamentos</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/fundamentos">Fundamentos</Nav.Link>
+                        <Nav.Link href="/clientes">Clientes</Nav.Link>
+                        <Nav.Link href="/array">Array</Nav.Link>
+                        <Nav.Link href="/objetos">Carros</Nav.Link>
+                        <Nav.Link href="/nomes">Nomes</Nav.Link>
+                        <Nav.Link href="/numeros">Números</Nav.Link>
+                        <Nav.Link href="/disney">Disney</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+            <div className="bg-secondary text-white text-center p-3">
+                <h1>{props.titulo}</h1>
+            </div>
+
+            <Container>
+                {/*props.children irá repassar nossas informações*/}
+                {props.children}
+
+            </Container>
+        
+        
+        </>
+
+       
+
+    )
+}
